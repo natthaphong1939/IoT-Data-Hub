99da611c1826866e064b299f75814339055a777b
natthaphong1939 <natthaphong.t@kkumail.com>
2025-02-21 16:45:49 +0700

    Replace greeting with button to open door in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold color-blue-500">
-        Hello world!
-      </h1>
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        Open door
+      </button>
     </>
   )
 }
