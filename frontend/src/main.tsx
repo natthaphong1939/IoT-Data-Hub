524858a71f30617bde2c2a11ca884273fedffa7b
dew <natthaphong.t@kkumail.com>
2025-03-14 19:49:04 +0700

    Update main.tsx to include AlertComponent and improve layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import AlertComponent from './component/Alert.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AlertComponent />
+    <main className="max-w-[1920px] m-auto min-h-screen bg-gray-200 p-4">
+      <h1 className='mb-4'>Room</h1>
+      <App />
+    </main>
   </StrictMode>,
 )
